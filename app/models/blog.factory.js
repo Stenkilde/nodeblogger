8098da7e43c665bb1777033d55ba85b124bc7853
Oliver Stenkilde <oliver@stenkilde.com>
2015-07-14 22:17:51 +0200

    Removed idea folder, added todo list and fixed the post call so we now can post blogs

--- app/models/blog.factory.js
+++ app/models/blog.factory.js
@@ -15,21 +15,12 @@
 
 		return service;
 
-		function addBlog() {
-			var deferred = $q.derfer();
+		function addBlog(article) {
+			var deferred = $q.defer();
 
 			var url = '/api/blog';
 
-			var storesPromise = $http({
-				method: 'POST',
-				url: url,
-				header: {'Content-type': 'application/json'},
-				data: {
-					header: vm.header,
-					body: vm.body,
-					created: vm.createdBy
-				}
-			}).then(function(results) {
+			var storesPromise = $http.post(url,article).then(function(results) {
 				deferred.resolve(results);
 			}, function(err) {
 				deferred.reject(err);
--- app/modules/blog/index/blog.controller.js
+++ app/modules/blog/index/blog.controller.js
@@ -10,6 +10,7 @@
 		/*jshint validthis: true */
 		var vm 		= this;
 		vm.data		= {};
+		vm.save		= save;
 
 		activate();
 
@@ -17,6 +18,14 @@
 			return BlogFactory.getBlogs().then(function(results) {
 				vm.data = results.data;
 			});
+		}
+
+		function save() {
+			BlogFactory.addBlog({
+				header: vm.header,
+				createdBy: vm.createBy,
+				body: vm.body
+			}).then(function(result) { console.log(result) });
 		}
 	}
 
